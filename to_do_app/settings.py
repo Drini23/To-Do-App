77c5aac0198c0360722f38c94953d482e98c2da9
Drini23 <122325981+Drini23@users.noreply.github.com>
2024-02-09 01:31:42 +0200

    again

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_2k08f1kyu5(&y@=r27ztnjtsh_(r43^-zvqyn@wd(cr@6qyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
