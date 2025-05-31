3f20c8293d658b55a3d6b7e789ceee39a7b48187
drini12345 <drini12345>
2025-06-01 01:15:38 +0200

    new update

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_2k08f1kyu5(&y@=r27ztnjtsh_(r43^-zvqyn@wd(cr@6qyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
